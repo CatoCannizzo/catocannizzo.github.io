993ef309785f693109733455f5860b5a95cf5d5c
cato cannizzo <catocannizzo@gmail.com>
2023-05-29 10:38:05 -0500

    Cleared directory to start work on portfolio

--- hcde439_PhysicalComputing/p5/ICA7(ReadingLine)/ICA7.ino
+++
@@ -1,9 +0,0 @@
-void setup() {
-  Serial.begin(9600);
-}
-
-void loop() {
-  int button = digitalRead(2);
-  Serial.println(button);
-  delay(50);
-}
--- hcde439_PhysicalComputing/py/serialprint.py
+++
@@ -1,7 +0,0 @@
-import serial
-portName = '/dev/ttyACM0'
-
-ser = serial.Serial(portName, 9600)
-
-while True:
-  print ser.readline()
