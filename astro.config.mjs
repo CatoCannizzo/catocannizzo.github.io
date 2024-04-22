228ca0ab4c08e4c787ddc9cb3321a74e5a85d06d
Cato Cannizzo <catocannizzo@gmail.com>
2024-04-22 14:29:06 -0400

    Added deploy yaml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import tunnel from "astro-tunnel";
 
 // https://astro.build/config
