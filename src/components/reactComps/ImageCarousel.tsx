70df1e0c8332673772f96a0e25c99e6876166948
Cato Cannizzo <catocannizzo@gmail.com>
2024-04-25 11:27:28 -0400

    Added a couple new todos, and made Header sticky

--- src/components/reactComps/ImageCarousel.tsx
+++ src/components/reactComps/ImageCarousel.tsx
@@ -19,7 +19,7 @@ interface Props {
 }
 
 const ImageCarousel: React.FC<Props> = ({ imgArrayOpt }) => {
-  console.log(imgArrayOpt);
+  // console.log(imgArrayOpt);
   return (
     <Carousel className="w-full max-w-xs">
       <CarouselContent>
--- src/components/reactComps/MobileNav.tsx
+++ src/components/reactComps/MobileNav.tsx
@@ -2,6 +2,7 @@
 import Navbar from "./Navbar";
 import { Drawer, DrawerContent, DrawerTrigger } from "../ui/drawer";
 import { MenuIcon } from "lucide-react";
+// !!! Make this icon apear in theme
 
 export default function NavMenu() {
   return (
