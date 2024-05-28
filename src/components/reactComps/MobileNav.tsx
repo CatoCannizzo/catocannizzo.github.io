11801bf702c36385eeb37a013315c38d13848871
Cato Cannizzo <catocannizzo@gmail.com>
2024-05-28 12:13:06 -0400

    Updated mobile hamburger to be in theme

--- src/components/reactComps/MobileNav.tsx
+++ src/components/reactComps/MobileNav.tsx
@@ -1,14 +1,24 @@
 import Navbar from "./Navbar";
 import { Drawer, DrawerContent, DrawerTrigger } from "../ui/drawer";
-import { MenuIcon } from "lucide-react";
-// !!! Make this icon apear in theme
-
+import "./Hamburger.css";
 export default function NavMenu() {
   return (
     <div className="visibile sm:hidden">
       <Drawer direction="right">
-        <DrawerTrigger>
-          <MenuIcon aria-label="Menu" />
+        <DrawerTrigger className="HamButton">
+          <svg
+            viewBox="0 0 100 100"
+            width="40"
+            height="40"
+            className="hamburger fill-none stroke-accent">
+            <path
+              className="line"
+              stroke-width="7"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m 20 20 l 60 60 a 1 1 0 0 1 -60 -60 h 60 a 1 1 0 0 1 0 30 h -60 a 1 1 0 0 0 0 30 h 60 h -60 a 1 1 0 0 1 60 -60 l -60 60"
+            />
+          </svg>
         </DrawerTrigger>
         <DrawerContent>
           <div className="my-auto ">
